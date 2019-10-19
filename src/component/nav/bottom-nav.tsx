2395acde2a16e7c87afb6bca6fca48aae049ee88
Benjamin Hood <Benjamin.Hood@rkkin.com>
2019-10-19 15:41:41 +0200

    some styling

--- src/component/nav/bottom-nav.tsx
+++ src/component/nav/bottom-nav.tsx
@@ -1,9 +1,33 @@
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export interface IBottomNavComponentProps {}
 
 export default class BottomNavComponent extends React.Component<IBottomNavComponentProps> {
   public render() {
-    return <div>Nav</div>;
+    return (
+      <nav className="bottom-nav">
+        <div className="bottom-nav__item">
+          <NavLink exact to={'/'} className="bottom-nav__item-link" activeClassName="is-active">
+            Diary
+          </NavLink>
+        </div>
+        <div className="bottom-nav__item">
+          <NavLink exact to={'/medication'} className="bottom-nav__item-link" activeClassName="is-active">
+            Medication
+          </NavLink>
+        </div>
+        <div className="bottom-nav__item">
+          <NavLink exact to={'/findings'} className="bottom-nav__item-link" activeClassName="is-active">
+            Findings
+          </NavLink>
+        </div>
+        <div className="bottom-nav__item">
+          <NavLink exact to={'/community'} className="bottom-nav__item-link" activeClassName="is-active">
+            Community
+          </NavLink>
+        </div>
+      </nav>
+    );
   }
 }
