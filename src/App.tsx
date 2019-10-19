dc1096bb80623bd2a58c829a5d7b17ffb5e0536f
Christian Meyer <christian.meyer@curry-group.com>
2019-10-19 18:29:03 +0200

    added entry creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import MainPage from './component/main';
 import { BrowserRouter as Router } from 'react-router-dom';
+import config from 'devextreme/core/config';
+
+config({
+  floatingActionButtonConfig: {
+    position: {
+      at: 'right bottom',
+      my: 'right bottom',
+      offset: '-27 -90'
+    }
+  }
+});
 
 const App: React.FC = () => {
   return (
--- src/component/modules/diary.tsx
+++ src/component/modules/diary.tsx
@@ -1,45 +1,93 @@
 import * as React from 'react';
 import bless from 'backendless';
+import moment from 'moment';
+import { SpeedDialAction } from 'devextreme-react/speed-dial-action';
+import { Button } from 'devextreme-react';
 
 export interface IDiaryPageProps {
   currentDate: number;
 }
 
 export default class DiaryPage extends React.Component<IDiaryPageProps> {
+  textareaRef: React.RefObject<HTMLTextAreaElement> = React.createRef();
+
   state = {
+    addNew: false,
+    processing: false,
     entries: []
   };
 
   loadData = async (date: number) => {
     const dataQuery = bless.DataQueryBuilder.create()
       .setSortBy('entrydate DESC')
-      .setWhereClause('entrydate >= ' + date + 'and entrydate');
+      .setWhereClause(
+        'entrydate >= ' +
+          date +
+          'and entrydate < ' +
+          moment(date)
+            .add(1, 'd')
+            .valueOf()
+      );
     const entries = await bless.Data.of('diaryentries').find(dataQuery);
     this.setState({ entries });
   };
 
   componentDidMount() {
-    // this.loadData();
+    this.loadData(this.props.currentDate);
   }
 
-  componentWillReceiveProps(nextProps: IDiaryPageProps) {
-    if (this.props.currentDate !== nextProps.currentDate) {
-      // this.setState({ currentDate: nextProps.currentDate });
-      this.loadData(nextProps.currentDate);
+  lastCurrentDate = 0;
+  componentDidUpdate(nextProps: IDiaryPageProps) {
+    if (this.lastCurrentDate !== this.props.currentDate) {
+      this.loadData(this.props.currentDate);
+      this.lastCurrentDate = this.props.currentDate;
     }
   }
 
+  onAddEntryButtonClicked = () => {
+    this.setState({ addNew: true });
+  };
+
+  onCancelButtonClicked = () => {
+    this.setState({ addNew: false });
+  };
+
+  onSaveButtonClicked = async () => {
+    if (this.textareaRef.current) {
+      this.setState({ processing: true });
+      const newEntry = {
+        entrydate: moment().valueOf(),
+        entry: this.textareaRef.current.value
+      };
+      await bless.Data.of('diaryentries').save(newEntry);
+      this.loadData(this.props.currentDate);
+      this.setState({ addNew: false });
+      this.setState({ processing: false });
+    }
+  };
+
   public render() {
     return (
-      <ul>
-        {this.state.entries.map((value: any) => {
-          return (
-            <li key={value.objectId}>
-              {value.entrydate} - {value.entry}
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        {this.state.addNew && (
+          <div>
+            <textarea ref={this.textareaRef}></textarea>
+            <Button onClick={this.onCancelButtonClicked}>Cancel</Button>
+            <Button onClick={this.onSaveButtonClicked}>Save</Button>
+            {this.state.processing && <span>Doing things ... </span>}
+          </div>
+        )}
+        <ul>
+          {this.state.entries.map((value: any) => {
+            return (
+              <li key={value.objectId}>
+                {value.entrydate} - {value.entry}
+              </li>
+            );
+          })}
+        </ul>
+        <SpeedDialAction icon={'add'} onClick={this.onAddEntryButtonClicked}></SpeedDialAction>
+      </div>
     );
   }
 }
