21e4c409c6b70fcfb6027ea1ab93e6ede7c79eec
Benjamin Hood <Benjamin.Hood@rkkin.com>
2019-10-19 14:42:28 +0200

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form-signin">
+        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label htmlFor="inputEmail" className="sr-only">
+          Email address
+        </label>
+        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+        <label htmlFor="inputPassword" className="sr-only">
+          Password
+        </label>
+        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me
+          </label>
+        </div>
+        <button className="btn btn-lg btn-primary btn-block" type="submit">
+          Sign in
+        </button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
